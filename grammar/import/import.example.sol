8fc0c5528aae5b5255b5d1c2c52d564a47b43842
Nils Martel <nilsmartel@yahoo.de>
2022-03-07 12:29:57 +0100

    Simplify some grammar

---
+++ grammar/import/import.example.sol
@@ -0,0 +1,12 @@
+# vim: set syntax=julia:
+use @std.collections *
+use @std.collections (Array, Map, Set)
+
+use @std.collection         (Map)
+use @std.collection.utils   (HashMap)
+
+
+# relative import, not used.
+# reason: complexity / discourage deep nesting
+use .utils  (joinAll)
+
