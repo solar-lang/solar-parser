a35b1010dbe96bcff59cb68acb2dd2f1484b642c
Nils Martel <nilsmartel@yahoo.de>
2023-04-20 14:18:47 +0200

    Include tests for grammar

---
+++ syntax-tests/use_5.sol
@@ -0,0 +1,17 @@
+use std.(io collections networking)
+
+use std.collections.HashMap
+
+use std..
+
+use std.(
+    collections.(
+        HashMap
+        BTreeMap
+        Array       
+    )
+
+    io..
+
+    debug..
+)
