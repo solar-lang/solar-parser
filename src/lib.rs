0220d52d2d151066060c739786974c5505c26779
Nils Martel <nilsmartel@yahoo.de>
2023-04-20 14:27:01 +0200

    Write new test system

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,37 @@ pub mod ast;
 pub(crate) mod util;
 
 mod parse;
-pub use parse::Parse;
 pub use ast::Ast;
+pub use parse::Parse;
+
+#[cfg(test)]
+mod syntax_tests {
+    use super::*;
+
+    #[test]
+    fn all_files() {
+        let testdir = "./syntax_tests";
+
+        let entries = std::fs::read_dir(testdir).expect("to find directory with syntax test files");
+
+        for e in entries {
+            let e = e.expect("read dir entry");
+
+            let filename = e.file_name().into_string().unwrap();
+
+            if !filename.ends_with(".sol") {
+                continue;
+            }
+
+            let content = std::fs::read_to_string(&filename).expect("read content of file");
+
+            let result = Ast::parse_ws(&content);
+
+            let (rest, _ast) = result.expect("To parse Ast");
+
+            let rest = rest.trim();
+
+            assert_eq!(rest, "", "expect parser to consume input");
+        }
+    }
+}
